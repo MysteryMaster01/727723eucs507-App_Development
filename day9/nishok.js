517737d5ab36415a5b343119e7ba4c26fc8cf9dd
MysteryMaster01 <727723eucs507@skcet.ac.in>
2024-08-06 15:40:10 +0530

    day9 nishok

---
+++ day9/nishok.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const nishok = () => {
+  return (
+    <View>
+      <Text>nishok</Text>
+    </View>
+  )
+}
+
+export default nishok
+
+const styles = StyleSheet.create({})
